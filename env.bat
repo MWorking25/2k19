1c81cb75ea433b0eb4c7025eef2ed1e22e99d749
Mayur Mhatre <mhatremayur2520@gmail.com>
2019-04-24 14:25:46 +0530

    Initialize project

---
+++ env.bat
@@ -0,0 +1,8 @@
+set environment=development
+set port=3800
+set encryption_key=&ndkidn_tyghs;psisst-bsss@8d9ns;lnsdihdkdbn_$
+set jwt_secret=@hir6yhfbi0bf99b03h83n9((bn88e83bh&jd-2(bndbd0hnidjk2ne0w7db2egmd82jbs72ndio^&
+set msql_host=localhost
+set msql_user=root
+set msql_pass=
+set db=unity
