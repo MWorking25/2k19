368f15630cbfa15948445b36b84588c805ce3fd6
Mayur Mhatre <mhatremayur2520@gmail.com>
2019-05-29 18:41:09 +0530

    drive backup

--- projects/unity/config/DBBackup.js
+++ projects/unity/config/DBBackup.js
@@ -1,6 +1,10 @@
 var schedule = require('node-schedule');
 var mysqldump  = require('mysqldump');
+var googleDrive = require('google-drive');
 
+// Authorization code : 1/BmtVQ_fZbJ7ra3bmdPdUgPlYL84g57lFLQkeEEo2xSk
+// refresh token : 4/WQHHKAPbZMVfW_f2TJeYV3kkjsiHdQjJyB6JreHFxVs4XjAGyO6kkzzh8iwp2S58-Mq7Vui3lAmwFR5ThY2ZkOA
+var token = 'ya29.GlsYB8QFRjnv-kaPPzqHuS6gZPvLIaIXSQZcNwZHenMd_jWRVBAL0FkGxyqmzq5rAyGy0AqXQkP-dNDT20ZxYnYwLHpIgXsEInsaOxDDaQLYFrMh7h5ry_rhrQ7f' //access token
 
 var j = schedule.scheduleJob('0 0 * * *', function(){
     mysqldump({
@@ -12,4 +16,8 @@ var j = schedule.scheduleJob('0 0 * * *', function(){
         },
         dumpToFile: './DbBackup/Unity/'+process.env.DB_NAME+'.sql',
     });
+
+
+    //googleDrive(token).files().insert(meta, params, callback)
+
   });
